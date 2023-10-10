726b2469351bd7b1acb0769460669229a4495867
dashoes15 <daltonasali@gmail.com>
2023-10-10 09:23:05 -0400

    lab 6 extra credit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int cpuNum;
+        int userGuess;
+        boolean done = false;
+        Scanner scan = new Scanner(System.in);
+        cpuNum = (int)(Math.random() * 10) + 1;
+        System.out.println("WOAH! The computer has made a random number 1-10. Try to guess!!!!");
+        do {
+            System.out.println("Enter your guess:");
+            if (scan.hasNextInt()) {
+                userGuess = scan.nextInt();
+                if (userGuess > 0 && userGuess < 11) {
+                    System.out.println("The cpu guess was: " + cpuNum);
+                    if (cpuNum == userGuess) {
+                        System.out.println("You were, RIGHT ON DA MONEYYY$$$");
+                    }else if (cpuNum > userGuess){
+                        System.out.println("You were too low :(");
+                    }else if (userGuess > cpuNum){
+                        System.out.println("You were too high :o");
+                    }
+                    done = true;
+                }else{
+                    System.out.println("The number you entered was not in the range!");
+                    scan.nextLine();
+                }
+            }else{
+                System.out.println("You have entered something invalid!");
+                scan.nextLine();
+            }
+        } while (!done);
     }
 }
